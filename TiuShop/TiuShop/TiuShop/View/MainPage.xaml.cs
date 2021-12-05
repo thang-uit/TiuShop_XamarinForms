6d9823976d3e9de5c300c843ea2167094f6aeaca
Chu Nam Thắng <namthangmu123@gamil.com>
2021-12-06 01:25:38 +0700

    1:25 6/12/2021

--- TiuShop/TiuShop/TiuShop/View/MainPage.xaml.cs
+++ TiuShop/TiuShop/TiuShop/View/MainPage.xaml.cs
@@ -8,7 +8,7 @@ using Xamarin.Forms;
 
 namespace TiuShop
 {
-    public partial class MainPage : ContentPage
+    public partial class MainPage : TabbedPage
     {
         public MainPage()
         {
--- TiuShop/TiuShop/TiuShop/obj/Debug/netstandard2.0/View/MainPage.xaml.g.cs
+++ TiuShop/TiuShop/TiuShop/obj/Debug/netstandard2.0/View/MainPage.xaml.g.cs
@@ -14,7 +14,7 @@ namespace TiuShop {
     
     
     [global::Xamarin.Forms.Xaml.XamlFilePathAttribute("View\\MainPage.xaml")]
-    public partial class MainPage : global::Xamarin.Forms.ContentPage {
+    public partial class MainPage : global::Xamarin.Forms.TabbedPage {
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
         private void InitializeComponent() {
