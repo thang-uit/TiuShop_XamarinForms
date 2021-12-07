acd3a26ed6fb80d3dd1b3d354a7e2a923f2d6936
Chu Nam Thắng <namthangmu123@gamil.com>
2021-12-08 00:32:59 +0700

    00:33 8/12/2021

--- TiuShop/TiuShop/TiuShop/obj/Debug/netstandard2.0/View/TabSearchPage.xaml.g.cs
+++ TiuShop/TiuShop/TiuShop/obj/Debug/netstandard2.0/View/TabSearchPage.xaml.g.cs
@@ -17,8 +17,12 @@ namespace TiuShop.View {
     public partial class TabSearchPage : global::Xamarin.Forms.ContentPage {
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
+        private global::Xamarin.Forms.Entry edtSearch;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
         private void InitializeComponent() {
             global::Xamarin.Forms.Xaml.Extensions.LoadFromXaml(this, typeof(TabSearchPage));
+            edtSearch = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Entry>(this, "edtSearch");
         }
     }
 }
