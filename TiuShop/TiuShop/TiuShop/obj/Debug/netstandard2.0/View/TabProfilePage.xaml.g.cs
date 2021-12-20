bf2ff5911308d3910c6c35abeddc5f825bc95791
Chu Nam Thắng <namthangmu123@gamil.com>
2021-12-21 01:00:19 +0700

    01:00 21/12/2021

--- TiuShop/TiuShop/TiuShop/obj/Debug/netstandard2.0/View/TabProfilePage.xaml.g.cs
+++ TiuShop/TiuShop/TiuShop/obj/Debug/netstandard2.0/View/TabProfilePage.xaml.g.cs
@@ -17,8 +17,12 @@ namespace TiuShop.View {
     public partial class TabProfilePage : global::Xamarin.Forms.ContentPage {
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
+        private global::Xamarin.Forms.Label lblName;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
         private void InitializeComponent() {
             global::Xamarin.Forms.Xaml.Extensions.LoadFromXaml(this, typeof(TabProfilePage));
+            lblName = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Label>(this, "lblName");
         }
     }
 }
